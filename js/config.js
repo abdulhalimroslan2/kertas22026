93be9ad9a48c30f429eddd2823ba225aad0b9a48
halimroslan <abdulhalimroslan@gmail.com>
2026-08-25 21:26:33 +0800

    Muat naik fail PDF E-Book Versi Pelajar (4.7MB) dan Versi Guru Skema Lengkap (4.0MB)

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const APP_CONFIG = {
         name: "E-Book Soalan Kertas 2 Topikal Percubaan 2026",
         description: "Format PDF berkualiti tinggi, soalan topikal mengikut bab Tingkatan 4 & 5",
         filename: "E-Book_Fizik_Percubaan_2026_Kertas2.pdf",
-        size: "4.8 MB",
+        size: "4.7 MB",
         type: "pdf",
         url: "assets/ebook-fizik-percubaan-2026-kertas2.pdf",
         badge: "Modul Soalan"
@@ -32,7 +32,7 @@ const APP_CONFIG = {
         name: "Skema & Analisis Jawapan Lengkap Kertas 2",
         description: "Panduan pemarkahan terperinci, tip skor A+, dan jalan kerja langkah demi langkah",
         filename: "Skema_Jawapan_Lengkap_Fizik_2026.pdf",
-        size: "3.2 MB",
+        size: "4.0 MB",
         type: "pdf",
         url: "assets/skema-jawapan-lengkap-fizik-2026.pdf",
         badge: "Skema & Tip A+"
