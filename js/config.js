6de58424bae4b3c3afe3a873056468c03b8b14e6
halimroslan <abdulhalimroslan@gmail.com>
2026-09-02 10:38:44 +0800

    Kemas kini Versi Guru (Skema Jawapan Lengkap 8.4 MB / 69 m/s) dengan pembetulan terperinci & meterai keselamatan dinamik

--- js/config.js
+++ js/config.js
@@ -30,9 +30,9 @@ const APP_CONFIG = {
       {
         id: "skema_jawapan",
         name: "Skema & Analisis Jawapan Lengkap Kertas 2",
-        description: "Panduan pemarkahan terperinci, tip skor A+, dan jalan kerja langkah demi langkah (Edisi ERATA Terkini)",
+        description: "Panduan pemarkahan terperinci, tip skor A+, dan jalan kerja langkah demi langkah (Edisi Kemas Kini Lengkap)",
         filename: "Skema_Jawapan_Lengkap_Fizik_2026.pdf",
-        size: "6.5 MB",
+        size: "8.4 MB",
         type: "pdf",
         url: "assets/skema-jawapan-lengkap-fizik-2026.pdf",
         badge: "Skema & Tip A+"
